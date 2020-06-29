b1ba16e61c355e633dfebdc7eb28fa2031ce497b
pragya2211 <44156976+pragya2211@users.noreply.github.com>
2020-06-30 00:10:20 +0530

    basic

--- ssh-pub.php
+++ ssh-pub.php
@@ -7,26 +7,34 @@
         <div class="news_body">
         <ul>
 <p>
-			<?php
-			require_once 'Classes/PHPExcel.php';
 
-			$excel = PHPExcel_IOFactory::load('publications_2020.xlsx');
-			$excel->setActiveSheetIndex(0);
-			echo "<table borders>";
-			$i = 1;
-			while($excel->getActiveSheet()->getCell('A'.$i)->getValue()!= "")
-			{
-			  $name = $excel->getActiveSheet()->getCell('A'.$i)->getValue();
-			  echo "
-			    <tr>
-			      <td> ".$name." </td>
-			    </tr>
-			  ";
-			  $i++;
-			}
 
-			echo "</table borders>";
-			?>
+			<table>
+			<tr>
+				<td><b><u>A. Khan, S. Bhalla</b></u>, “Communication Challenges for Sustainability: Variations in Hindi and English Media Coverage of Urban Environment Issues,” in Institute of Public Enterprise International Conference, April 2020.</td>
+			</tr>
+			<tr>
+
+				<td><b><u>Mrinmoy Chakrabarty, Makoto Wada</u></b>. Perceptual effects of fast and automatic visual ensemble statistics from faces in individuals with typical development and autism spectrum conditions. Scientific Reports [Nature Publishing Group], 10 (2169), 2020.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Nishad Patnaik</u></b>,Forthcoming: Heidegger and the Politics of Essence", translated into Bengali, in Ebong Mushayera</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Paro Mishra</u></b>, Forthcoming. Mobile Phones, Family Connections, and Family Conflicts: Ethnographic Realities from the Lives of Cross-region Migrant Brides in North India. In Lakshmi Lingam and Nolwazi Mkhwanazi (Eds.) Smart Phones, Social Media and Gender Faultlines: Southern Perspectives on Emerging Perspectives. Routledge</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Paro Mishra</u></b>, Forthcoming. (With Ravinder Kaur). Gender Imbalance, Marriage Squeeze and Multiple Biological Clocks: Exploring the Challenges to Intergenerational Contract in North India. Special Issue on Trapped in the Hourglass: Time, Reproductive Decline and the Problematization of Ageing. Anthropology and Aging.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Ray, S. B., Mishra, M. V., & Srinivasan, N.</u></b> (2020). Attentional blink with emotional faces depends on emotional expressions: a relative positive valence advantage. Cognition and Emotion, 1-20.</td>
+			</tr>
+
+			</table>
 		</p>
 	</ul>
 </div>
@@ -38,26 +46,96 @@
         <div class="news_body">
         <ul>
 <p>
-			<?php
-			require_once 'Classes/PHPExcel.php';
+			<table>
+			<tr>
+				<td><b><u>Manohar Kumar</b></u>,Speaking Truth to Power. A Theory of Whistleblowing with Daniele Santoro. Springer. 2018. “A Justification of Whistleblowing”, Philosophy and Social Criticism, with Daniele Santoro. Vol 43, No. 7, 2017.</td>
+			</tr>
+			<tr>
 
-			$excel = PHPExcel_IOFactory::load('publications_before2020.xlsx');
-			$excel->setActiveSheetIndex(0);
-			echo "<table borders>";
-			$i = 1;
-			while($excel->getActiveSheet()->getCell('A'.$i)->getValue()!= "")
-			{
-			  $name = $excel->getActiveSheet()->getCell('A'.$i)->getValue();
-			  echo "
-			    <tr>
-			      <td> ".$name." </td>
-			    </tr>
-			  ";
-			  $i++;
-			}
+				<td><b><u>Manohar Kumar</u></b>, “A Right to Protection of Whistleblowers”, with Daniele Santoro, in Archibugi, D and A, Benli (eds.). Claiming Rights in Europe: A European Citizenship in Practice. Routledge, Taylor and Francis. 2017..</td>
+			</tr>
 
-			echo "</table borders>";
-			?>
+			<tr>
+				<td><b><u>Khan, Aasim. Arora, Gaurav. and Khan, Zoya. </u></b>“Toxic talk and Environment Action: Understanding news and social media engagement around air pollution issues in Delhi”. Accepted under the panel: In the time of toxic air: knowledge, politics, urban futures. At International Sociological Association (ISA) RC21 Annual Conference, Delhi. September 2019.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Arora, G., Feng, H., Anderson, C.J., Hennessy, D.A.</u></b>,2020. Evidence of climate change impacts on crop comparative advantage and land use. Agricultural Economics, 51(2): 221-236.</td>
+			</tr>
+
+			<tr>
+				<td><b><u> Arora, G., Wolter, P.T.</u></b>, 2018. Tracking land cover change along the western edge of the U.S. Corn Belt from 1984 through 2016 using satellite sensor data: Observed trends and contributing factors. Journal of Land Use Science, 13(1-2): 59-80.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Gayatri Nair</u></b>, 'Representations of Rape in Popular Culture: Gone Girl and Badlapur' (Co-authored with Dipti Tamang). In International Feminist Journal of Politics, Volume 18, Issue 4, 14th December, 2016; pp. 614-618.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Gayatri Nair</u></b> ,'The Bitter Aftertaste of the Beef Ban; Choice, Caste and Consumption'. In Economic and Political Weekly, Volume 51, Issue 10, 2016; pp 14-15. ISSN-0012-9976</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Kanjilal Kirti </u></b>,“Endogenous Equity Shares in Cournot Competition: Welfare Analysis and Policy.” - Kiriti Kanjilal and Felix Munoz-Garcia. (Published online ahead of print in The B.E. Journal of Economic Analysis and Policy).Kanjilal Kirti,“Endogenous Equity Shares in Cournot Competition: Welfare Analysis and Policy.” - Kiriti Kanjilal and Felix Munoz-Garcia. (Published online ahead of print in The B.E. Journal of Economic Analysis and Policy).</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Masakazu Ide, Takeshi Atsumi, Reiko Fukatsu, Mrinmoy Chakrabarty </u></b>, Effects of emotion cues on visual temporal resolution in individuals with autism spectrum disorders. 42 nd Annual Meeting of Japan Neuroscience Society, Niigata, JAPAN – July 25, 2019</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Mrinmoy Chakrabarty, M.Wada.</u></b> , Perception of Visual Ensemble Statistics of Faces in Autism and Typically Developing individuals. 42 nd Annual Meeting of Japan Neuroscience Society, Niigata, JAPAN – July 26, 2019</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Nishad Patnaik </u></b>, “Indeterminacy and Identity: Kant and Husserl on Moral Consciousness”, ed. Ranjan Kr. Panda, to be published by the Department of Humanities and Social Sciences, IIT Bombay.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Nishad Patnaik</u></b> , “Science and the Promise of Objective Knowledge: Husserl’s Intersubjective Renewal”, in Problems of the Self: Philosophical Writings in Consciousness, Subjectivity and Otherness, ed. Manidipa Sen, Aakar Books, New Delhi, 2019.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Paro Mishra</u></b> , 2018. Being ‘bare branches’: Demographic imbalance, marriage exclusion and masculinity in North India’. In Sharada Srinivasan and Li Shuzhou (eds.) ‘Scarce’ Women and ‘Surplus’ Men in China and India: Macro Demographics versus Local Dynamics. Canada: Springer.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Paro Mishra</u></b> , 2016. Imbalanced sex-ratio and cross-region marriage: The challenges of transcending caste boundaries. In Ravinder Kaur (ed.) Too Many Men, Too Few Women: Mapping the Adverse Consequences of Imbalanced Sex-ratios in India and China. New Delhi: Orient Blackswan.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Praveen Priyadarshi</u></b> , “Letter from Gurgaon” LSE South Asia Blog, January 21, 2013.Link</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Praveen Priyadarshi</u></b> , “Gujarat: How an Exclusionary Political Pact is also a Durable One” LSE South Asia Blog, December 3, 2012.Link</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Smriti Singh</u></b> , Authored “Becoming a Critical Pedagogue: Teacher Agency and Critical Theory” in a book titled ‘Dynamics of Inclusive Classroom: Social Diversity, Inequality and School Education in India’ published by the Orient Blackswan in September 2017. ISBN number is -978-93-52870-13-4.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Smriti Singh</u></b> , Authored“Discourses of Caste in the Classroom: Reflections from Under- Graduate Teaching in a Delhi University College” in a book titled ‘Accessing Higher Education: Footprints of Marginalised Groups’ published by Aakar Books, New Delhi in June, 2017. ISBN number is -978-93-5002-485-0.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Mishra, M. V., Ray, S. B., & Srinivasan, N.</u></b>  (2018). Cross-cultural emotion recognition and evaluation of Radboud faces database with an Indian sample. PloS one, 13(10), e0203959..</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Ray, S. B.</u></b> (2018). Applications of Neuroscience for Managing Affective State at Workplace. NHRD Network Journal, 0974173918799138.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Souvik Dutta</u></b> , Ethnic Conflicts with Informed agents: A Cheap Talk Game with Multiple Audiences (with Pathikrit Basu and Suraj Shekhar), 2019, Economics Letters.</td>
+			</tr>
+
+			<tr>
+				<td><b><u>Souvik Dutta</u></b> , Do Social Networks facilitate the Spread of Ponzi Schemes? Evidence from a Primary Survey (with Abhirup Sarkar), 2019, Economic and Political Weekly.</td>
+			</tr>
+
+			</table>
 		</p>
 	</ul>
 </div>
